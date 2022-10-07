8d6b842300dc3479b3a4ea7f161fd5842070ed5c
Your Name <you@example.com>
2022-10-07 07:35:30 +0500

    Day_4 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,14 +14,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         themeMode: ThemeMode.dark,
-        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        theme: ThemeData(
+            primarySwatch: Colors.deepPurple,
+            fontFamily: GoogleFonts.lato().fontFamily),
         darkTheme: ThemeData(primarySwatch: Colors.blueGrey),
-        initialRoute: "/home",
         routes: {
           "/": (context) => const LoginPage(),
           "/home": (context) => const HomePage(),
           "/login": ((context) => const LoginPage()),
-        }
-        );
+        });
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,15 +9,55 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        // ignore: unnecessary_const
-        child: const Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 40, fontWeight: FontWeight.bold, color: Colors.indigo),
-          textScaleFactor: 1.5,
-        ),
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login_image.png",
+            fit: BoxFit.cover,
+            scale: 2.5,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text("Welcome to this App",
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.deepPurple,
+                  fontWeight: FontWeight.bold)),
+          SizedBox(height: 20.0),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter passwaord",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Hi Codepur");
+                    }),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
